be98ade95d7197de5e2c21bc5fa26d6fdfc6c9fd
kobylan <koba1575>
2020-06-07 23:00:25 +0600

    sidemenu

--- src/store/components/Sidebar/index.js
+++ src/store/components/Sidebar/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { sidebarConfig } from "../../../configs/sidebarConfig";
 import logo from "../../../assets/icons/logo.svg";
+import logout from "../../../assets/icons/logout.svg";
 const Sidebar = () => {
   return (
-    <div style={{ height: "100vh" }} className="col">
-      <img src={logo} alt="na|teste" />
-      <ul className="col-2 list-group">
+    <div className="col-2 h-100 d-flex flex-column h5  justify-content-between">
+      <div>
+        <img className="ml-2 p-1" width={170} src={logo} alt="na|teste" />
         {sidebarConfig.map((item) => (
-          <li className="list-group-item d-flex border-0 align-items-center">
+          <div className="wmaxc hover pointer  m-2 pl-2 pr-2 p-1 d-flex align-items-center">
             <span>{item.icon}</span>
-            <span className="p-1"> {item.title}</span>
-          </li>
+            <span className="ml-1"> {item.title}</span>
+          </div>
         ))}
-      </ul>
-      <div className="col justify-content-end">
-        <span className="p-1">Выйти</span>
+      </div>
+      <div className="wmaxc hover pointer d-flex mb-2 ml-2 pl-2 pr-2 p-1 align-items-center">
+        <span>
+          <img src={logout} width={40} alt="Выйти" />
+        </span>
+        <span className="ml-1">Выйти</span>
       </div>
     </div>
   );
