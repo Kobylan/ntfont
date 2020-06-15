f555b9d87f94a12cb9af6f18d32f02d09648ec43
nos43ratu <kairolla_adil2@mail.ru>
2020-06-15 23:36:57 +0300

    123

--- src/pages/Cooker/FindOrders/FindOrder.js
+++ src/pages/Cooker/FindOrders/FindOrder.js
@@ -2,29 +2,40 @@ import React, { useEffect, useState } from "react";
 import Order from "./Order";
 import * as axios from "axios";
 import Loading from "../../../store/components/Loading";
+import useInfiniteScroll from "../../../store/components/useInfiniteScroll";
 
 const FindOrder = () => {
-  const [orders, setOrders] = useState("loading");
+  const [orders, setOrders] = useState([]);
+  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
+
+  const fetchData = async () => {
+    const result = await axios("https://nateste.herokuapp.com/api/orders");
+    setOrders((prevState) => Object.assign(prevState, result.data));
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios("https://nateste.herokuapp.com/api/orders");
-      setOrders(result.data);
-    };
     fetchData();
   }, []);
+  function fetchMoreListItems() {
+    setTimeout(() => {
+      fetchData();
+      setIsFetching(false);
+    }, 5000);
+  }
 
   return (
     <div className="middle-content ">
       <div>FILTER ELEMENT</div>
       <div className="">
-        {orders === "loading" ? (
-          <Loading />
-        ) : orders === [] ? (
-          "No orders"
-        ) : (
-          orders.map((order, i) => <Order key={i} order={order} />)
-        )}
+        {
+          (console.log(orders),
+          orders === [] ? (
+            <Loading />
+          ) : (
+            orders.map((order, i) => <Order key={i} order={order} />)
+          ))
+        }
+        {isFetching && "Fetching more list items..."}
       </div>
     </div>
   );
