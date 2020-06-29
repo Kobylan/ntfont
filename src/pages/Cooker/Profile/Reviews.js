3e35b04d4dd0b5aa6d8a375525620f8df2152e0f
nos43ratu <kairolla_adil2@mail.ru>
2020-06-30 01:22:40 +0300

    avatar bug fix

--- src/pages/Cooker/Profile/Reviews.js
+++ src/pages/Cooker/Profile/Reviews.js
@@ -3,12 +3,14 @@ import React from "react";
 export const Reviews = (props) => {
   return (
     <div className="d-flex align-items-start mb-3 review-card">
-      <img
-        width="100%"
-        className="profile-avatar shadow mr-2"
-        src={props.data.avatar}
-        alt="Adil genius"
-      />
+      <div className="profile-avatar shadow mr-2">
+        <img
+          width="100%"
+          className="br-3"
+          src={props.data.avatar}
+          alt="Adil genius"
+        />
+      </div>
       <div className="review-content  p-3 d-flex flex-column text-break">
         <div className="d-flex align-item-center justify-content-between border-bottom">
           <div>{props.data.name}</div>
