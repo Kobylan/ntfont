3e4be00141372fd715afc487356a3e2ae49c44cf
kobylan <koba1575>
2020-07-01 02:20:08 +0600

    mayby

--- src/pages/Cooker/Orders/Chat/Chat.js
+++ src/pages/Cooker/Orders/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { history } from "../../../../history";
 import { ReactComponent as Back } from "../../../../assets/icons/back.svg";
@@ -165,6 +165,16 @@ const Chat = () => {
     },
   ];
   const { userID } = useParams();
+  const messagesEndRef = React.createRef();
+  const [scrolled, setScrolled] = useState(false);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView();
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+    setScrolled(true);
+  }, []);
   return (
     <div className="d-flex flex-column flex-grow-1  chat-container ">
       <div className="chat-back">
@@ -177,19 +187,26 @@ const Chat = () => {
         </div>
       </div>
       <div className="chat-body d-flex flex-column justify-content-between br-3">
-        <div className="chat-info flex-column">
+        <div className="chat-info flex-column ">
           <div className="chat-info-username">Adil</div>
-          <div className="chat-avatar"></div>
+          <div className="chat-avatar" />
         </div>
-        <div className="chat-message-content">
+
+        <div className={`chat-message-content ${!scrolled && "invisible"}`}>
           {messeges.map((message) => (
             <Message message={message} />
           ))}
+          <div style={{ float: "left", clear: "both" }} ref={messagesEndRef} />
         </div>
 
         <div className="chat-send-message">
           <input type="text" className="chat-input" />
-          <div className="btn btn-success">send</div>
+          <div
+            className="btn btn-success"
+            onClick={() => console.log(messagesEndRef)}
+          >
+            send
+          </div>
         </div>
       </div>
     </div>
