72e154b3df3557263a0121cf94931cd8a9cacd61
nos43ratu <kairolla_adil2@mail.ru>
2020-07-11 17:27:53 +0300

    useAPI

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const useApi = (params) => {
-  const { method, url, request } = params;
-  const API = `https://thawing-reef-32246.herokuapp.com/api/${url}/`;
+const useApi = (request) => {
+  const { url, method = "GET", params, body, infScroll = false } = request;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  const trigger = infScroll ? request : "";
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    axios({
+      method: method,
+      url: `https://thawing-reef-32246.herokuapp.com/api/${url}/`,
+      body: body,
+      params: params,
+    })
+      .then((res) => {
+        infScroll
+          ? setData((prevOrders) => {
+              prevOrders.concat(res.data.results);
+              setHasMore(res.data.next !== null);
+            })
+          : setData(res.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+        setLoading(false);
+      });
+  }, [trigger]);
+  return { loading, error, data, hasMore };
 };
 
 export default useApi;
