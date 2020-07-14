8262c1c6ff4a964dc13835419ce47a698075f6f3
nos43ratu <kairolla_adil2@mail.ru>
2020-07-14 20:17:28 +0300

    slider

--- src/components/InputRange.js
+++ src/components/InputRange.js
@@ -1,31 +1,51 @@
 import React, { useRef, useState } from "react";
 
 const InputRange = () => {
-  let draggingLeft = false;
+  const [drag, setDrag] = useState(false);
   const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(20);
   const leftRef = useRef();
+  const rightRef = useRef();
   const trackRef = useRef();
-  const trackWidth = trackRef.current?.offsetWidth;
-  const trackLeft = trackRef.current?.offsetLeft;
-  const leftWidth = leftRef.current?.offsetWidth;
-  const maxRight = trackWidth - leftWidth;
-  const grabOrGrabbing = draggingLeft ? "grabbing" : "grab";
+  let draggingLeft = false;
+  let draggingRight = false;
+  const root = document.getElementById("root").style;
   window.addEventListener("mouseup", () => {
     draggingLeft = false;
-    document.getElementById("root").style.userSelect = "auto";
-    document.getElementById("root").style.cursor = "auto";
+    draggingRight = false;
+    setDrag(false);
+    root.userSelect = "auto";
+    root.cursor = "auto";
   });
 
   window.addEventListener("mousemove", (e) => {
+    root.userSelect = "none";
     if (draggingLeft) {
-      document.getElementById("root").style.userSelect = "none";
-      const offset = e.clientX - 10 - trackLeft;
+      const offset = e.clientX - 10 - trackRef.current?.offsetLeft;
       if (offset < 0) {
         setLeft(0);
-      } else if (offset > maxRight) {
-        setLeft(maxRight);
+      } else if (
+        offset >
+        trackRef.current?.offsetWidth - leftRef.current?.offsetWidth
+      ) {
+        setLeft(trackRef.current?.offsetWidth - leftRef.current?.offsetWidth);
       } else {
         setLeft(offset);
+      }
+    }
+    if (draggingRight) {
+      const offset = e.clientX - 10 - trackRef.current?.offsetLeft;
+      if (offset < 0) {
+        setRight(0);
+      } else if (
+        offset >
+        trackRef.current?.offsetWidth - leftRef.current?.offsetWidth - left
+      ) {
+        setRight(
+          trackRef.current?.offsetWidth - leftRef.current?.offsetWidth - left
+        );
+      } else {
+        setRight(offset);
       }
     }
   });
@@ -36,15 +56,31 @@ const InputRange = () => {
       ref={trackRef}
     >
       <div
-        className="border-radius-50 w-20px h-20px bg-blue position-absolute "
+        className={`border-radius-50 w-20px h-20px bg-blue position-absolute cursor-${
+          drag ? `grabbing` : `grab`
+        }`}
         ref={leftRef}
         style={{
           left: `${left}px`,
-          cursor: `${grabOrGrabbing}`,
         }}
         onMouseDown={() => {
           draggingLeft = true;
-          document.getElementById("root").style.cursor = "grabbing";
+          setDrag(true);
+          root.cursor = "grabbing";
+        }}
+      />
+      <div
+        className={`border-radius-50 w-20px h-20px bg-dark position-absolute cursor-${
+          drag ? `grabbing` : `grab`
+        }`}
+        ref={rightRef}
+        style={{
+          left: `${right}px`,
+        }}
+        onMouseDown={() => {
+          draggingRight = true;
+          setDrag(true);
+          root.cursor = "grabbing";
         }}
       />
     </div>
