4ce043a80a70ba791dc1befc2323f9e6624cccff
kobylan <koba1575>
2020-07-14 22:02:37 +0600

    input range

--- src/components/InputRange.js
+++ src/components/InputRange.js
@@ -9,31 +9,38 @@ const InputRange = () => {
   const trackLeft = trackRef.current?.offsetLeft;
   const leftWidth = leftRef.current?.offsetWidth;
   const maxRight = trackWidth - leftWidth;
+
+  window.addEventListener("mouseup", () => {
+    setDraggingLeft(false);
+    console.log(draggingLeft);
+  });
+
+  window.addEventListener("mousemove", (e) => {
+    if (draggingLeft) {
+      const offset = e.clientX - 10 - trackLeft;
+      if (offset < 0) {
+        setLeft(0);
+      } else if (offset > maxRight) {
+        setLeft(maxRight);
+      } else {
+        setLeft(offset);
+      }
+    }
+  });
+
   return (
     <div
       className="h-15px bg-white-gray m-20 d-flex align-items-center rounded position-relative"
       ref={trackRef}
-      onMouseMove={(e) => {
-        if (draggingLeft) {
-          const offset = e.clientX - trackLeft;
-          if (offset < 0) {
-            setLeft(0);
-          } else if (offset > maxRight) {
-            setLeft(maxRight);
-          } else {
-            setLeft(offset);
-          }
-        }
-      }}
     >
       <div
         className="border-radius-50 w-20px h-20px bg-blue position-absolute"
         ref={leftRef}
         style={{ left: `${left}px` }}
         onMouseDown={(e) => {
+          console.log("asd");
           setDraggingLeft(true);
         }}
-        onMouseUp={() => setDraggingLeft(false)}
       />
       <div
         onClick={() => setLeft(20)}
